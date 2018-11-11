5f7f8999b12baa412de754f455568c0d109ad115
Shawn Wu <1040375843@qq.com>
2018-11-12 00:07:20 +0800

    Future example

---
+++ concurrency/src/main/java/com/enyi/concurrency/example/aqs/FutureExample.java
@@ -0,0 +1,39 @@
+package com.enyi.concurrency.example.aqs;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.*;
+
+/**
+ * @author enyi.lr
+ * @date 2018/11/11 7:20 PM
+ * @description
+ */
+@Slf4j
+public class FutureExample {
+
+    static class MyCallable implements Callable<String> {
+
+        @Override
+        public String call() throws Exception {
+            log.info("do something in callable");
+            Thread.sleep(5000);
+            return "Done";
+        }
+    }
+
+
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        // 通过Future接收Callable返回的值s
+        Future<String> future = executorService.submit(new MyCallable());
+        log.info("do something in main");
+        Thread.sleep(1000);
+        String result = future.get();
+        log.info("result：{}", result);
+
+
+    }
+
+
+}
---
+++ concurrency/src/main/java/com/enyi/concurrency/example/aqs/FutureTaskExample.java
@@ -0,0 +1,33 @@
+package com.enyi.concurrency.example.aqs;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.FutureTask;
+
+/**
+ * @author enyi.lr
+ * @date 2018/11/11 7:25 PM
+ * @description FutureTaskExample
+ */
+@Slf4j
+public class FutureTaskExample {
+    public static void main(String[] args) throws Exception {
+        FutureTask<String> futureTask = new FutureTask<>(new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                log.info("do something in callable");
+                Thread.sleep(5000);
+                return "Done";
+            }
+        });
+
+        new Thread(futureTask).start();
+        log.info("do something in main");
+        Thread.sleep(1000);
+        String result = futureTask.get();
+        log.info("result：{}", result);
+
+
+    }
+}
