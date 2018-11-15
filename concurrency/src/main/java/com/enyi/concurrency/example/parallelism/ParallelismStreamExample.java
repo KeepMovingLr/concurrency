8097f6fe46829d0083bc76ec1eb5e028954ef1ad
enyi.lr <enyi.lr@alibaba-inc.com>
2018-11-15 18:05:05 +0800

    parallelism

---
+++ concurrency/src/main/java/com/enyi/concurrency/example/parallelism/ParallelismStreamExample.java
@@ -0,0 +1,10 @@
+package com.enyi.concurrency.example.parallelism;
+
+/**
+ * @author enyi.lr
+ * @date 15/11/2018 6:03 PM
+ * @description https://docs.oracle.com/javase/tutorial/collections/streams/parallelism.html
+ */
+public class ParallelismStreamExample {
+
+}
